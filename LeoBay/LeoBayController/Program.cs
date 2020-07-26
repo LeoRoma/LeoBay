3a2aeb2f588c1a2ab37f2eec1aec426075c4c6c4
LeoRoma <xiajtn@gmail.com>
2020-07-26 15:42:09 +0100

    Added MVC's files

---
+++ LeoBay/LeoBayController/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeoBayController
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
