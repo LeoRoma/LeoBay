841f4a38d0366aa6a5012ea2b979733edc576585
LeoRoma <xiajtn@gmail.com>
2020-07-27 21:26:27 +0100

    LoginController added

--- LeoBay/LeoBayController/RegisterController.cs
+++ LeoBay/LeoBayController/RegisterController.cs
@@ -9,19 +9,10 @@ namespace LeoBayController
 {
     public class RegisterController
     {
-        // set password
-        public const string strPassword = "LetMeIn99$";
-
-        // set permutations
-        public const String strPermutation = "ouiveyxaqtd";
-        public const Int32 bytePermutation1 = 0x19;
-        public const Int32 bytePermutation2 = 0x59;
-        public const Int32 bytePermutation3 = 0x17;
-        public const Int32 bytePermutation4 = 0x41;
-        static void Main(string[] args)
-        {
-            //Console.WriteLine(GetName());
-        }
+        //static void Main(string[] args)
+        //{
+        //    //Console.WriteLine(GetName());
+        //}
 
         public void CreateNewUser(string firstName, string lastName, string email, string password)
         {
@@ -46,25 +37,6 @@ namespace LeoBayController
             cs.Write(inputByteArray, 0, inputByteArray.Length);
             cs.FlushFinalBlock();
             return Convert.ToBase64String(ms.ToArray());
-        }
-
-        public string Decrypt(string str)
-        {
-            str = str.Replace(" ", "+");
-            string DecryptKey = "2013;[pnuLIT)WebCodeExpert";
-            byte[] byKey = { };
-            byte[] IV = { 18, 52, 86, 120, 144, 171, 205, 239 };
-            byte[] inputByteArray = new byte[str.Length];
-
-            byKey = System.Text.Encoding.UTF8.GetBytes(DecryptKey.Substring(0, 8));
-            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
-            inputByteArray = Convert.FromBase64String(str.Replace(" ", "+"));
-            MemoryStream ms = new MemoryStream();
-            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(byKey, IV), CryptoStreamMode.Write);
-            cs.Write(inputByteArray, 0, inputByteArray.Length);
-            cs.FlushFinalBlock();
-            System.Text.Encoding encoding = System.Text.Encoding.UTF8;
-            return encoding.GetString(ms.ToArray());
         }
     }
 }
