bae9a9250f49fe279bb078cec44be3bb2cf15874
LeoRoma <xiajtn@gmail.com>
2020-07-28 10:07:47 +0100

    Login and register test passed

--- LeoBay/LeoBayController/LoginController.cs
+++ LeoBay/LeoBayController/LoginController.cs
@@ -62,26 +62,9 @@ namespace LeoBayController
 
         public string GetCurrentUser()
         {
-            return $"{_currentUser.CurrentFirstName} {_currentUser.CurrentLastName}";
+            return $"Welcome To LeoBay {_currentUser.CurrentFirstName} {_currentUser.CurrentLastName}";
         }
 
-        public string Decrypt(string str)
-        {
-            str = str.Replace(" ", "+");
-            string DecryptKey = "2013;[pnuLIT)WebCodeExpert";
-            byte[] byKey = { };
-            byte[] IV = { 18, 52, 86, 120, 144, 171, 205, 239 };
-            byte[] inputByteArray = new byte[str.Length];
-
-            byKey = System.Text.Encoding.UTF8.GetBytes(DecryptKey.Substring(0, 8));
-            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
-            inputByteArray = Convert.FromBase64String(str.Replace(" ", "+"));
-            MemoryStream ms = new MemoryStream();
-            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(byKey, IV), CryptoStreamMode.Write);
-            cs.Write(inputByteArray, 0, inputByteArray.Length);
-            cs.FlushFinalBlock();
-            System.Text.Encoding encoding = System.Text.Encoding.UTF8;
-            return encoding.GetString(ms.ToArray());
-        }
+        
     }
 }
